21899482554cfc11343e0d86f777a23ed0ea903d
Harrison Wynn <harrisonwynn42@gmail.com>
2017-05-21 15:52:48 -0500

    debugging backtracking function

--- gridPrinter.py
+++ gridPrinter.py
@@ -176,18 +176,15 @@ class Grid(object):
 				self.nodes[A][B] = (max(x, y, d)); #adding to the Grid object instance
 		return S[self.height-1][self.width-1];
 
-	def recBackCall(self):
-		return self.recBacktrace(self.width-1, self.height-1);
-		
 	def recBacktrace(self, x, y, CPath=[[]]):
 		#make new list to put our old list into (copies if needed)
 		pathList = copy.deepcopy(CPath); #we will append the current position to all paths in this
-		print("list of paths: ");
+		#print("list of paths: ");
 		for path in pathList:
 			path.append([x,y]);
-			print(path);
-		print("distance from origin at current position",self.nodes[y][x]);
-		print("characters at current position:",self.word1[x], self.word2[y]);
+			#print(path);
+		print("x:", x, " y:", y);
+		#print("characters at current position:",self.word1[x], self.word2[y]);
 		
 		nextPaths = []; #we will append entire paths to this
 		if(x==0 and y==0):
@@ -196,27 +193,63 @@ class Grid(object):
 		#did we come from above?
 		if(y!=0):
 			if(self.nodes[y][x] == self.nodes[y-1][x]):
-				print(x);
-				print(y-1);
-				print(pathList);
-				print(self.recBacktrace(x,(y-1), pathList));
-				#nextPaths = nextPaths + self.recBacktrace([x,y-1, pathList]);
-		print(nextPaths);
-		
-		return pathList;
-			
+				nextPaths = copy.deepcopy(nextPaths) + copy.deepcopy(self.recBacktrace(x,y-1, pathList));
+				"""if(x==4 and y==5):
+					print("inside conditional");
+					print("pathList: ");
+					for path in pathList:
+						print(path);
+					print("nextPaths: ");
+					for path in nextPaths:
+						print(path);
+					print("copy.deepcopy(nextPaths): ", copy.deepcopy(nextPaths));
+					print("copy.deepcopy(self.recBacktrace(x,y-1, pathList)): ", self.recBacktrace(x,y-1, pathList));"""
 		#did we come from the left?
-			
-		#did we come diagonally?
-			
+		if(x!=0):
+			if(self.nodes[y][x] == self.nodes[y][x-1]):
+				nextPaths = copy.deepcopy(nextPaths) + copy.deepcopy(self.recBacktrace(x-1,y, pathList));
 
+				"""if(x==4 and y==5):
+					print("inside conditional");
+					print("pathList: ");
+					for path in pathList:
+						print(path);
+					print("nextPaths: ");
+					for path in nextPaths:
+						print(path);
+					print("copy.deepcopy(nextPaths): ", copy.deepcopy(nextPaths));
+					print("copy.deepcopy(self.recBacktrace(x,y-1, pathList)): ", self.recBacktrace(x,y-1, pathList));"""
+		
+		#did we come diagonally?
+		if(x!=0 and y!=0):
+			if(self.nodes[y][x] == (self.nodes[y-1][x-1])+1):
+				nextPaths = copy.deepcopy(nextPaths) + copy.deepcopy(self.recBacktrace(x-1,y-1, pathList));
+				"""if(x==4 and y==5):
+					print("inside conditional");
+					print("pathList: ");
+					for path in pathList:
+						print(path);
+					print("nextPaths: ");
+					for path in nextPaths:
+						print(path);
+					print("copy.deepcopy(nextPaths): ", copy.deepcopy(nextPaths));
+					print("copy.deepcopy(self.recBacktrace(x,y-1, pathList)): ", self.recBacktrace(x,y-1, pathList));"""
+		#debugging
+		"""if(x==4 and y==5):
+			print("pathList: ");
+			for path in pathList:
+				print(path);
+			print("nextPaths: ");
+			for path in nextPaths:
+				print(path);"""
+		return copy.deepcopy(nextPaths);
 
 butt = Grid("ATGTTAT","ATCGTAC");
 butt.dynGridA();
 butt.tinyprintGrid();
 
-butt.recBacktrace(5,5,[[[5,6]]]);
+
 print("");
-#butt.recBacktrace(5,6);
-#butt.recBacktrace(5,6, [[[7,7],[6,7],[6,6],[5,5]],[[7,7],[7,6],[6,6],[5,5]]]);
-butt.recBackCall();
+butt.recBacktrace(7,7);
+print("");
+#butt.recBacktrace(4,5, [[[7,7],[6,7],[6,6],[5,5]],[[7,7],[7,6],[6,6],[5,5]]]);
