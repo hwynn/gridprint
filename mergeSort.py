5bfe02ec4ec375f44bbff00b5d61237d3046d922
Harrison Wynn <harrisonwynn42@gmail.com>
2017-06-15 14:09:56 -0500

    made mergesort. It works now. I'll have to figure out how this can apply to linear space alignment.

--- mergeSort.py
+++ mergeSort.py
@@ -1,38 +1,39 @@
 #!/usr/bin/python
 
-
-def divide(pList):
+def complexDivide(pList):
 	if(len(pList)<2):
 		return(pList, []);
 	elif((len(pList)%2)!=0):
-		return( pList[0:int((len(pList)+1)/2)], pList[int((len(pList)+1)/2):int(len(pList))]);
+		return(pList[0:int((len(pList)+1)/2)], pList[int((len(pList)+1)/2):int(len(pList))]);
 	else:
 		return((pList[0:int(len(pList)/2)]), (pList[int(len(pList)/2):int(len(pList))]));
 
-"""l= [0, 1, 2, 3, 4, 5, 6];
-len(l) = 7;
-(len(l)+1)/2 = 4;
+def combine(sListA, sListB):
+	newList = [];
+	A = sListA.copy();
+	B = sListB.copy();
+	while(len(A)!=0 and len(B)!=0):
+		#print("A:", A);
+		#print("B: ", B);
+		#print(newList);
+		if(A[0] < B[0]):
+			newList.append(A.pop(0));
+		else:
+			newList.append(B.pop(0));
+	newList = newList + A;
+	newList = newList + B;
+	return newList;
 
-la = [0, 1, 2, 3];
-la = l[0:5]
-lb = [4, 5, 6];
+def mergeSort(c):
+	n = len(c);
+	if(n==1):
+		return c;
+	left, right = complexDivide(c);
+	sortedLeft = mergeSort(left);
+	sortedRight = mergeSort(right);
+	sortedList = combine(sortedLeft ,sortedRight);
+	return sortedList;
 
-l = [0, 1, 2];
-len(l) = 3;
-((len(l)+1)/2) = 2;
-la = [0, 1];
-la = l[0:2];
-la = l[0:((len(l)+1)/2)];
-lb = l[2:3];
-lb = l[((len(l)+1)/2):len(l)];
-
-l= [0, 1, 2, 3, 4, 5];
-la = [0, 1, 2];
-la = l[0:(len(l)/2)];
-
-lb = [3, 4, 5];"""
-
-myList = [0, 1, 2, 3, 4];
-print(myList);
-print(divide(myList)[0]);
-print(divide(myList)[1]);
+#print(combine([2,4,5,7],[1,2,3,6]));
+#print(combine([5],[2]));
+print(mergeSort([2,4,5,7,1,2,3,6]));
